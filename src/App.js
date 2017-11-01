a535da773fb0799227cb79c246997d3b3af764b4
姜峰 <jiang.f@Ctrip.com>
2017-11-01 10:45:20 +0800

    updata

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ const Thead = () => (
   <thead>
     <tr>
       <td>
+        <input type="checkbox" />
+      </td>
+      <td>
         文件/目录名
               </td>
       <td>
@@ -25,7 +28,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('/path?path=./')
+    let path = './'
+    fetch(`/path?path=${path}`)
       .then(data => data.json())
       .then(json => {
         this.setState({
@@ -39,14 +43,31 @@ class App extends Component {
       fileList
     } = this.state
     return (
-      <section>
+      <section className="am-container">
+        <div className="am-form" >
+          <div class="am-form-group">
+            <input type="text" className="am-form-field" placeholder="输入目录" />
+          </div>
+          <div class="am-form-group">
+            <button className="am-btn am-btn-primary">确定</button>
+          </div>
+          <div class="am-form-group">
+            <input type="text" className="am-form-field" placeholder="输入新的文件名" />
+          </div>
+          <div class="am-form-group">
+            <button className="am-btn am-btn-warning">修改</button>
+          </div>
+        </div>
         <table className="am-table am-table-striped am-table-hover">
           <Thead />
           <tbody>
             {
               !!fileList.length && fileList.map(({ name, size, isDirectory, mtime }) => {
                 return (
-                  <tr>
+                  <tr>  
+                    <td>
+                      <input type="checkbox" />
+                    </td>
                     <td>
                       {isDirectory && <a href="###">{name}</a>}
                       {!isDirectory && <span>{name}</span>}
@@ -75,12 +96,12 @@ class App extends Component {
 
   formatSize = data => {
     let tempSize = ''
-    let kb = data/1024
-    if(kb < 1 ){
+    let kb = data / 1024
+    if (kb < 1) {
       return data + 'B'
-    } else if( kb >=1 && kb < 1024 ){
+    } else if (kb >= 1 && kb < 1024) {
       return Math.floor(kb) + 'KB'
-    } else if(kb >= 1024 ) {
+    } else if (kb >= 1024) {
       // let mb = kb / 1024
       // if(){
 
